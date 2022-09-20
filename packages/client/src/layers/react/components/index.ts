aa06d4fa9e973e98c4ef479322542649886fdf4d
alvrs <alvarius@lattice.xyz>
2022-09-20 15:33:44 +0100

    feat: add inventory

--- packages/client/src/layers/react/components/index.ts
+++ packages/client/src/layers/react/components/index.ts
@@ -1,9 +1,11 @@
 import { registerComponentBrowser } from "./ComponentBrowser";
 import { registerActionQueue } from "./ActionQueue";
 import { registerLoadingState } from "./LoadingState";
+import { registerInventory } from "./Inventory";
 
 export function registerUIComponents() {
   registerLoadingState();
   registerComponentBrowser();
   registerActionQueue();
+  registerInventory();
 }
