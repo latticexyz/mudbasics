a543c31091afc6973303f71671450d4f4d6ca73b
pwrstudio <pwrpntr@gmail.com>
2022-10-26 11:02:01 +0200

    feat: add move system

---
+++ packages/contracts/src/systems/MoveSystem.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+import "solecs/System.sol";
+import { IWorld } from "solecs/interfaces/IWorld.sol";
+import { getAddressById } from "solecs/utils.sol";
+
+import { PositionComponent, ID as PositionComponentID, Coord } from "../components/PositionComponent.sol";
+
+uint256 constant ID = uint256(keccak256("system.Move"));
+
+contract MoveSystem is System {
+  constructor(IWorld _world, address _components) System(_world, _components) {}
+
+  function execute(bytes memory arguments) public returns (bytes memory) {
+    (uint256 entity, Coord memory targetPosition) = abi.decode(arguments, (uint256, Coord));
+
+    PositionComponent position = PositionComponent(getAddressById(components, PositionComponentID));
+    position.set(entity, targetPosition);
+  }
+
+  function executeTyped(uint256 entity, Coord memory targetPosition) public returns (bytes memory) {
+    return execute(abi.encode(entity, targetPosition));
+  }
+}
