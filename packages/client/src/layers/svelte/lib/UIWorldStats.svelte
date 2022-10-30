0339d51cdea6795d5d9134a736db1300c65b8c63
pwrstudio <pwrpntr@gmail.com>
2022-10-30 18:40:23 +0100

    feat: show entity id in log

--- packages/client/src/layers/svelte/lib/UIWorldStats.svelte
+++ packages/client/src/layers/svelte/lib/UIWorldStats.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
   import { entities } from "../stores/entities";
+  import { layers } from "../stores/layers";
+
+  function shorten(s: string) {
+    return s.slice(0, 4) + "..." + s.slice(-4);
+  }
 </script>
 
 <div class="ui-world-stats">
   {#each Object.entries($entities) as [id, value]}
     <div>
-      Entity <strong>{id}</strong> is at: x => {value.position.x} y => {value.position.y}
+      <strong>{shorten($layers.network.world.entities[id])}</strong>
+      is at: x => {value.position.x}
+      y => {value.position.y}
     </div>
   {/each}
 </div>
