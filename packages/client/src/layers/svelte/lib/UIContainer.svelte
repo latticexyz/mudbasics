5656b51f20acb3849491c8ff981c202bf4d55b7e
pwrstudio <pwrpntr@gmail.com>
2022-11-07 17:02:56 +0100

    feat: add view UI component

--- packages/client/src/layers/svelte/lib/UIContainer.svelte
+++ packages/client/src/layers/svelte/lib/UIContainer.svelte
@@ -6,6 +6,7 @@
   import UICharacterStats from "./UICharacterStats.svelte";
   import UIOperationsEditor from "./UIOperationsEditor.svelte";
   import UIMap from "./UIMap.svelte";
+  import UIView from "./UIView.svelte";
 </script>
 
 <div class="ui-container">
@@ -23,6 +24,11 @@
     <!-- ***** CHARACTER STATS -->
     <UIComponent title="Character Stats">
       <UICharacterStats />
+    </UIComponent>
+
+    <!-- ***** VIEW -->
+    <UIComponent title="View">
+      <UIView />
     </UIComponent>
 
     <!-- ***** WORLD STATS -->
