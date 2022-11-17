90f511f41bcde1b8014d3b7d6b25a284ba88e810
pwrstudio <pwrpntr@gmail.com>
2022-11-17 13:28:37 +0100

    feat: initial work on operations countdown

--- packages/client/src/layers/svelte/lib/UIVisualOperationsEditor.svelte
+++ packages/client/src/layers/svelte/lib/UIVisualOperationsEditor.svelte
@@ -11,6 +11,8 @@
   let turnCounter = 0;
   let activeOperationIndex = 0;
   let filteredSequence: Operation[];
+  let oldCoolDownBlock = 0;
+  let operationDuration = 0;
 
   const emptyOperation: Operation = {
     name: "+",
@@ -58,6 +60,15 @@
   }
 
   blockNumber.subscribe((newBlock) => {
+    if ($entities[$playerAddress].coolDownBlock !== oldCoolDownBlock) {
+      operationDuration = $entities[$playerAddress].coolDownBlock - newBlock + 1;
+      // Tween value down from operationDuration to 0 over operationDuration seconds
+      progress.set(operationDuration, { duration: 0 });
+      progress.set(0, { duration: operationDuration * 1000 });
+
+      oldCoolDownBlock = $entities[$playerAddress].coolDownBlock;
+    }
+
     // Execute the next operation if
     // – Sequencer is activated
     // - Cooldown period is over
@@ -85,16 +96,26 @@
           class:failure={!sequenceSuccess[index]}
           class:active={sequencerActive && activeOperationIndex == index}
         >
-          <div>
-            {operation.name}
-            {#if activeOperationIndex == index && operation.category !== "empty"}
-              <strong>
-                {#if $entities[$playerAddress].coolDownBlock - $blockNumber > 0}
-                  {$entities[$playerAddress].coolDownBlock - $blockNumber}
-                {/if}
-              </strong>
-            {/if}
+          <div class="operation-info">
+            <div class="operation-name">
+              {operation.name}
+            </div>
           </div>
+
+          {#if activeOperationIndex == index && operation.category !== "empty"}
+            <div class="operation-progress">
+              {#if $entities[$playerAddress].coolDownBlock - $blockNumber > 0 && $progress > 0}
+                <div>
+                  <progress value={$progress} max={operationDuration} />
+                </div>
+                <div>
+                  <strong>
+                    {$entities[$playerAddress].coolDownBlock - $blockNumber} seconds
+                  </strong>
+                </div>
+              {/if}
+            </div>
+          {/if}
         </div>
       </div>
     {/each}
@@ -198,30 +219,23 @@
   .slot-container {
     display: inline-block;
     width: 100px;
+    height: 100px;
     margin-right: 30px;
   }
 
   .slot {
     width: 100px;
     height: 100px;
-    display: inline-block;
-    text-align: center;
-    line-height: 100px;
-    font-size: 18px;
     display: flex;
+    font-size: 18px;
     justify-content: center;
     align-items: center;
     border: 2px solid transparent;
+    position: relative;
   }
 
   .hidden {
     opacity: 0;
-  }
-
-  .slot-progress {
-    font-size: 9px;
-    text-align: center;
-    margin-top: 5px;
   }
 
   .operation-grid {
@@ -243,7 +257,7 @@
   }
 
   .active {
-    border: 2px solid black;
+    border: 2px solid darkgray;
   }
 
   .cooldown-overlay {
@@ -264,5 +278,35 @@
 
   .ui-operations-editor {
     position: relative;
+  }
+
+  .operation-info {
+    text-align: center;
+  }
+
+  .operation-progress {
+    position: absolute;
+    bottom: 5px;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 9px;
+    text-align: center;
+  }
+
+  progress {
+    width: 80px;
+    height: 5px;
+    border-radius: 0;
+    margin-bottom: 5px;
+  }
+
+  progress::-webkit-progress-bar {
+    background: lightgray;
+    border-radius: 0;
+  }
+
+  progress::-webkit-progress-value {
+    background: grey;
+    border-radius: 0;
   }
 </style>
