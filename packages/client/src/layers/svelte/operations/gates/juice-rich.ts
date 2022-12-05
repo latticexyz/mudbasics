5ec01adfb9963fc98ecc30a3dcbac8276aea1d10
pwrstudio <pwrpntr@gmail.com>
2022-12-05 11:40:43 +0100

    feat: change gate values

--- packages/client/src/layers/svelte/operations/gates/juice-rich.ts
+++ packages/client/src/layers/svelte/operations/gates/juice-rich.ts
@@ -2,7 +2,7 @@ import { get } from "svelte/store";
 import { player } from "../../stores/player";
 
 export function juiceRich() {
-  if ((get(player).resource || 0) > 200) {
+  if ((get(player).resource || 0) >= 100) {
     return true;
   } else {
     return false;
