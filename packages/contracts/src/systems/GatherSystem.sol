0642ff8c56b6fdc33307645e4fe7736549edf1ad
pwrstudio <pwrpntr@gmail.com>
2022-12-05 10:13:08 +0100

    feat: set cooldown to 5 blocks

--- packages/contracts/src/systems/GatherSystem.sol
+++ packages/contracts/src/systems/GatherSystem.sol
@@ -142,7 +142,7 @@ contract GatherSystem is System {
     }
 
     energyComponent.set(entity, currentEnergyLevel - energyInput);
-    coolDownComponent.set(entity, int32(int256(block.number)) + energyInput);
+    coolDownComponent.set(entity, int32(int256(block.number)) + 5);
 
     // Check if dead
     if (currentEnergyLevel - energyInput <= 0) {
