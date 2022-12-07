eac04417c3e04d75fc5c92d31cb276195fb7ed6f
pwrstudio <pwrpntr@gmail.com>
2022-12-07 12:25:49 +0100

    feat: reduce initial resource balance

--- packages/contracts/src/constants.sol
+++ packages/contracts/src/constants.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0;
 
 int32 constant WORLD_HEIGHT = 50;
 int32 constant WORLD_WIDTH = 50;
-int32 constant INITIAL_RESOURCE = 80;
+int32 constant INITIAL_RESOURCE = 0;
 int32 constant INITIAL_ENERGY = 100;
 
 enum entityType {
